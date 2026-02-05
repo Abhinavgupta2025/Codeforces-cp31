4c2e5068e368a023f72b05bd4806f0e59a0e5692
Abhinav Gupta <abhinavgupta555251@gmail.com>
2026-02-05 23:46:46 +0530

    Solve todays question

---
+++ A_AvtoBus.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long wheels;
+        cin>>wheels;
+        if(wheels<4 || wheels%2==1){
+            cout<<-1<<endl;
+            continue;
+        }
+        long long maxwheels = wheels/4;
+        long long minwheels = (wheels + 5)/6;
+        cout<<minwheels<<" "<<maxwheels<<endl;
+    }
+    return 0;
+}
